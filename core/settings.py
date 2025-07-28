bc541560a53a3ebb655257af7697593a0ebb274c
leejd <heeo0oear@gmail.com>
2025-07-28 15:16:56 +0900

    upload 환경변수 설정

--- core/settings.py
+++ core/settings.py
@@ -44,10 +44,10 @@ class Settings(BaseSettings):
     youtube_api_key: str = Field(..., env="YOUTUBE_API_KEY")
 
     # == AWS ===
-    aws_access_key_id: str = Field(..., env="AWS_ACCESS_KEY_ID")
-    aws_secret_access_key: str = Field(..., env="AWS_SECRET_ACCESS_KEY")
-    aws_region: str = Field(..., env="AWS_REGION")
-    bucket_name: str = Field(..., env="BUCKET_NAME")
+    aws_access_key_id: str = Field(..., env="S3-ACCESS-KEY")
+    aws_secret_access_key: str = Field(..., env="S3-SECRET-ACCESS-KEY")
+    aws_region: str = Field(..., env="S3-REGION")
+    bucket_name: str = Field(..., env="S3-BUCKET-NAME")
 
     class Config:
         env_file = ".env"
